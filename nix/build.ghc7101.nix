d49ba0d7e5477e1cac84bd381ee9a3e6ebe101c3
Renzo Carbonara <gnuk0001@gmail.com>
2015-08-09 19:35:55 -0300

    nix

--- nix/build.ghc7101.nix
+++ nix/build.ghc7101.nix
@@ -5,14 +5,6 @@ with (import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; 
 
 let hs = haskell-ng.packages.ghc7101.override {
     overrides = self: super: {
-      instant-generics = haskell-ng.lib.overrideCabal super.instant-generics (drv: {
-        src = fetchFromGitHub {
-            owner = "k0001";
-            repo = "instant-generics";
-            rev = "3f46e7da4667bcdc0ed637c8141ace4330076862";
-            sha256 = "0h79xlws17cgcc4zwh9z95wscxa57gy9hc48mz7b7j1vipyzx1i5";
-        };
-      });
       instant-aeson = self.callPackage ./default.nix {};
     };
   };
